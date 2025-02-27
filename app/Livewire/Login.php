14368b432cd4713cfbbab3c5db2c067db4c0325e
Brem0317 <patrickbarcelo17@gmail.com>
2025-02-27 22:49:47 +0800

    update

--- app/Livewire/Login.php
+++ app/Livewire/Login.php
@@ -32,6 +32,7 @@ class Login extends LoginPage
     {
         return Checkbox::make('remember')
             ->required()
+            ->dehydrated(false)
             ->label(fn () => new HtmlString(
                 'I agree to PAO\'s <a href="'.route('policy.index').'" target="_blank" style="text-decoration: underline;">Terms and Conditions</a>. 
                 Read our <a href="'.route('policy.index').'" target="_blank" style="text-decoration: underline;">Privacy Policy</a>.'
